6a987bc10486a141fef4c7dc824071a491568b3a
dherrada <dylan.herrada@adafruit.com>
2021-01-12 12:45:46 -0500

    Ran pre-commit, added licenses

--- examples/il0398_simpletest.py
+++ examples/il0398_simpletest.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
 """Simple test script for 4.2" 400x300 black and white displays.
 
 Supported products:
